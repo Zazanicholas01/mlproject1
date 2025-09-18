2ca5ce0ab090308081abec80c78ba933275e6c83
Zazanicholas01 <matteoesposito12.me@gmail.com>
2025-09-18 17:52:31 +0200

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts', 'train.csv')
@@ -47,8 +50,12 @@ class DataIngestion:
             raise CustomException(e, sys)
 
 if __name__ == '__main__':
+
     obj = DataIngestion()
     train_path, test_path = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_path, test_path)
+    train_array, test_array, no = data_transformation.initiate_data_transformation(train_path, test_path)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_array, test_array))
