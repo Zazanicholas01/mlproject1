ee1b38dc4d27696a3467c02933cff29fd68342ef
Zazanicholas01 <matteoesposito12.me@gmail.com>
2025-09-19 11:10:18 +0200

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 
 from src.exception import CustomException
 
@@ -20,7 +20,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     """Train and evaluate models on the dataset"""
     
     try:
@@ -28,6 +28,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+
+            parameters = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, parameters, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
 
             model.fit(X_train, y_train)
 
@@ -38,6 +45,8 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
+
+            print(f"Evaluated model: {model} with score {test_model_score}")
 
         return report
     
